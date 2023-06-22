5ece33c7865a7f6e98000f6a30cd14ed06ebf263
Josue Machado Velasquez <66174344+JosueM3615@users.noreply.github.com>
2023-06-22 16:29:54 -0600

    Plantilla primer version

---
+++ resources/views/plantilla.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/estilos.css') }}" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+<body>
+<div class="container">
+    <div class="Panelderecha">
+      
+    <div class="presentacion">
+       <h4>AMW Solutions</h4>
+        <img src="{{ asset('img/logo.png') }}" class="img_logo" alt="">
+     </div>
+
+      <div class="opciones">
+        <ul>
+        <li class="menu_li"><a href="#">Usuarios</a></li>
+        <li class="menu_li"><a href="#">Agentes</a></li>
+        <li class="menu_li"><a href="#">Roles</a></li>
+        <li class="menu_li"><a href="#">Prioridades</a></li>
+        <li class="menu_li"><a href="#">Categorías</a></li>
+        <li class="menu_li"><a href="#">Etiquetas</a></li>
+        
+      </ul>
+    </div>
+
+    </div>
+    
+    <div class="panelizq">
+     
+    <div class="navbar">
+        <h1>HOLA GAYS</h1>
+      </div> 
+      <div class="contenidopanel">
+        <div class="formulario">
+            <form action="" class="contform">
+                <label for="titulo"> Titulo</label>
+                <input type="text" name="titulo">
+
+                <label for="descripcion"> Descripción</label>
+                <textarea name="descripcion" id="" cols="30" rows="10"></textarea>
+            </form>
+        </div>
+      </div>
+      
+    </div>
+
+  </div>
+</body>
+</html>
